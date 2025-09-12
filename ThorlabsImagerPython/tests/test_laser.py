bc6ccf7550f4cce88f2482b15c8fe001dd9788ae
Albert Chen <115337461+albertchen1375@users.noreply.github.com>
2025-09-12 13:03:42 -0700

    Added test files

---
+++ ThorlabsImagerPython/tests/test_laser.py
@@ -0,0 +1,10 @@
+import unittest
+from src.imager.laser import ThorlabsOCTLaser
+
+class TestThorlabsOCTLaser(unittest.TestCase):
+    def test_set_laser_state(self):
+        # TODO: Add device mock and test set_laser_state
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ ThorlabsImagerPython/tests/test_stage.py
@@ -0,0 +1,10 @@
+import unittest
+from src.imager.stage import ThorlabsOCTStage
+
+class TestThorlabsOCTStage(unittest.TestCase):
+    def test_move_to(self):
+        # TODO: Add device mock and test move_to
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
