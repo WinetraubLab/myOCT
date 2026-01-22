6a26929990d00f5cf5b76d22d703f959753b298d
alejoruge <alejoruge@gmail.com>
2026-01-22 15:51:48 -0800

    Use strcmpi to read ignoring capital letters

--- LoadSave/yOCTLoadInterfFromFile_ThorlabsHeaderLambda.m
+++ LoadSave/yOCTLoadInterfFromFile_ThorlabsHeaderLambda.m
@@ -1,4 +1,4 @@
-function dimensions = yOCTLoadInterfFromFile_ThorlabsHeaderLambda(inputDataFolder,OCTSystem,chirp)
+function dimensions = yOCTLoadInterfFromFile_ThorlabsHeaderLambda(inputDataFolder,octSystem,chirp)
 %This script figures out what is lambda of the data folder
 % Optional inputs, if they are unknown, we will figure them out
 %   chirp - if we have it (otherwise set to [])
@@ -6,22 +6,20 @@ function dimensions = yOCTLoadInterfFromFile_ThorlabsHeaderLambda(inputDataFolde
 %% Figure out basic Parameters
 
 %To get lambda min & max we contacted thorlabs
-switch(OCTSystem)
-    case {'Ganymede','Ganymede_SRR'}
-        chirpFileName = 'ChirpGanymede.dat';
-        lambdaMin = 796.23;%[nm] Manufacturer number: 824.16, See 2019-08-31GanymedeFilterCalibration Report for more details
-        lambdaMax = 1010.02;%[nm]Manufacturer number: 1038.03, See 2019-08-31GanymedeFilterCalibration Report for more details
-    case 'Gan632'
-        chirpFileName = 'ChirpGanymede.dat';
-        lambdaMin = 796; %[nm] TODO these will need to be measured, see 2019-08-31GanymedeFilterCalibration Report for more details
-        lambdaMax = 1010; %[nm] TODO these will need to be measured, see 2019-08-31GanymedeFilterCalibration Report for more details
-    case {'Telesto','Telesto_SRR'}
-        chirpFileName = 'ChirpTelesto.dat';
-        lambdaMin = 1208.69;%[nm] Manufacturer number: 1200.56, See 2018-12-04 Filter Calibration Report for more details
-        lambdaMax = 1372.50;%[nm] Manufacturer number: 1367.75, See 2018-12-04 Filter Calibration Report for more details
-    
-    otherwise
-        error('Unknown OCT System, please specify as input ''OCTSystem''')
+if strcmpi(octSystem, 'Ganymede') || strcmpi(octSystem, 'Ganymede_SRR')
+    chirpFileName = 'ChirpGanymede.dat';
+    lambdaMin = 796.23;%[nm] Manufacturer number: 824.16, See 2019-08-31GanymedeFilterCalibration Report for more details
+    lambdaMax = 1010.02;%[nm]Manufacturer number: 1038.03, See 2019-08-31GanymedeFilterCalibration Report for more details
+elseif strcmpi(octSystem, 'Gan632')
+    chirpFileName = 'ChirpGanymede.dat';
+    lambdaMin = 796; %[nm] TODO these will need to be measured, see 2019-08-31GanymedeFilterCalibration Report for more details
+    lambdaMax = 1010; %[nm] TODO these will need to be measured, see 2019-08-31GanymedeFilterCalibration Report for more details
+elseif strcmpi(octSystem, 'Telesto') || strcmpi(octSystem, 'Telesto_SRR')
+    chirpFileName = 'ChirpTelesto.dat';
+    lambdaMin = 1208.69;%[nm] Manufacturer number: 1200.56, See 2018-12-04 Filter Calibration Report for more details
+    lambdaMax = 1372.50;%[nm] Manufacturer number: 1367.75, See 2018-12-04 Filter Calibration Report for more details
+else
+    error('Unknown OCT System, please specify as input ''octSystem''')
 end
 
 %% Load Chirp File
